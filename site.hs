b514bb3f7ec3ddaa8c2a5be0e9424d71d4ab7cb6
Issa Rice <riceissa@gmail.com>
2014-09-14 02:02:08 -0700

    document all my research on hakyll options

--- site.hs
+++ site.hs
@@ -23,7 +23,22 @@ main = hakyll $ do
             {->>= loadAndApplyTemplate "templates/default.html" defaultContext-}
             {->>= relativizeUrls-}
 
+    -- See http://vapaus.org/text/hakyll-configuration.html#fnref1 .
     --let pandocOptions = defaultHakyllWriterOptions { writerHTMLMathMethod = MathJax "" }
+    --
+    -- See all the options for defaultHakyllWriterOptions here:
+    -- https://gist.github.com/shirataki/9538344 . To do all this, we
+    -- need to import Text.Pandoc.Options:
+    -- https://groups.google.com/forum/#!msg/hakyll/Fob_YFFh7kU/2uF3_Ihk3WoJ
+    -- .  As for how to actually produce a ToC, there is discussion
+    -- here:
+    -- https://groups.google.com/forum/#!msg/hakyll/FFTGC4TsXo8/mueGzxg5o-kJ
+    -- , which points to
+    -- https://github.com/jaspervdj/hakyll/blob/master/web/site.hs ;
+    -- there, look at how withToc is used as a writer passed to
+    -- pandocCompilerWith, and how withToc contains the options listed
+    -- below. I'm not exactly sure if all three are necessary, but just
+    -- setting writerTableOfContents to True did *not* work.
     let pandocOptions = defaultHakyllWriterOptions
              { writerTableOfContents = True
                , writerTOCDepth = 3
@@ -32,6 +47,17 @@ main = hakyll $ do
              }
 
     match "pages/*" $ do
+        -- Below, route is adapted from
+        -- http://ethanschoonover.com/source/bin/site.hs see the
+        -- function stripTopDir there. Essentially, we want the files in
+        -- pages to *not* clutter up our git repository's root
+        -- directory, but at the same time, when we're serving the
+        -- compiled files, we want them in the root directory of the
+        -- domain. Therefore, we first (from right to left) get the file
+        -- page, break it up into lists split by directories, remove the
+        -- first directory (the "page" directory) with tail, join it
+        -- back together, then drop the .html extension in favor of Cool
+        -- URIs.
         route $ customRoute $ dropExtension . joinPath . tail . splitPath . toFilePath
         -- route $ setExtension ""
         compile $ pandocCompilerWith defaultHakyllReaderOptions pandocOptions
