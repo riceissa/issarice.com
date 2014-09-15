0b71b3602dd5cb120a5dd2e39f790d1f3b3a6b54
Issa Rice <riceissa@gmail.com>
2014-09-14 19:29:18 -0700

    rename: archive -> all pages

--- site.hs
+++ site.hs
@@ -74,7 +74,7 @@ main = hakyll $ do
             {->>= loadAndApplyTemplate "templates/default.html" postCtx-}
             {->>= relativizeUrls-}
 
-    create ["archive.html"] $ do
+    create ["all"] $ do
         route idRoute
         compile $ do
             pages <- loadAll "pages/*"
