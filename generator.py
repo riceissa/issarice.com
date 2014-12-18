42f31994200232c5f32da80b43e1977b13ef1d7a
Issa Rice <riceissa@gmail.com>
2014-12-18 00:10:49 -0800

    switch back to production code; get rid of print statements

--- generator.py
+++ generator.py
@@ -42,7 +42,7 @@ SITE_DIR = "_site/"
 # relative to SITE_DIR
 TAGS_DIR = "tags/"
 
-pages_pat = "pages/if.md"
+pages_pat = "pages/*.md"
 pages_lst = [Filepath(i) for i in glob.glob(pages_pat)]
 
 # Copy css
@@ -58,7 +58,6 @@ for page_path in pages_lst:
     page = Page(page_path)
     print("Processing " + str(page.origin))
     page.load()
-    print page.metadata
     page.json = meta.organize_tags(page.json, tag_synonyms, tag_implications)
     page.metadata.update_with(meta.get_metadata_dict(page.json))
     tags_lst = page.metadata.tags
@@ -73,11 +72,9 @@ for page_path in pages_lst:
         css = Filepath("css/minimal.css").relative_to(Filepath(inter)).path,
         source = page.origin.path,
     )
-    print ctxp
     ctx = Metadata(**default_metadata.__dict__)
     ctx.update_with(page.metadata)
     ctx.update_with(ctxp)
-    print ctx
 
     env = Environment(loader=FileSystemLoader('.'))
     skeleton = env.get_template('templates/skeleton.html')
@@ -85,70 +82,66 @@ for page_path in pages_lst:
     for tag in ctx.tags:
         tags.append({'name': tag, 'path': to_unicode(Filepath(to_string(tag)).route_with(to_dir(TAGS_DIR)).path)})
     tags = sorted(tags, key=lambda t: t['name'])
-    final = skeleton.render(body=body, page=ctx, tags=tags, css=ctx.css)
+    final = skeleton.render(body=body, page=ctx, tags=tags, css=ctx.css, path="./")
     page_data.append((ctx.title, inter, tags_lst)) # to be used later
-    #command = "pandoc -f html -t json | python relativize_urls.py {origin} | pandoc -f json -t html".format(origin=page.origin.route_with(my_route).route_with(drop_one_parent_dir_route))
-    final = c.run_command("pandoc -s --parse-raw -f html -t json", pipe_in=to_string(final))
-    final = c.run_command("python relativize_urls.py index", pipe_in=to_string(final))
-    final = c.run_command("pandoc --parse-raw -f json -t html", pipe_in=to_string(final))
 
     with open(write_to.path, 'w') as f:
         f.write(to_string(final))
 
-#all_tags = list(set(all_tags))
+all_tags = list(set(all_tags))
 
-#for tag in all_tags:
-    ##print("Processing tag page for " + tag)
-    #pages = []
-    #for page_tuple in page_data:
-        #if tag in page_tuple[2]:
-            #pages.append({'title': to_unicode(page_tuple[0]), 'url': to_unicode("../" + page_tuple[1])})
-    #pages = sorted(pages, key=lambda t: t['title'])
-    #write_to = Filepath(SITE_DIR + TAGS_DIR + to_string(tag))
-    #ctx = Metadata(
-        #title = "Tag: " + tag,
-        #css = Filepath("css/minimal.css").relative_to(Filepath(TAGS_DIR + to_string(tag))).path,
-        #license = "cc0",
-    #)
-    #env = Environment(loader=FileSystemLoader('.'))
-    #page_list = env.get_template('templates/page-list.html')
-    #body = to_unicode(page_list.render(pages=pages))
-    #skeleton = env.get_template('templates/skeleton.html')
-    #final = skeleton.render(body=body, page=ctx, css=ctx.css)
+for tag in all_tags:
+    print("Processing tag page for " + tag)
+    pages = []
+    for page_tuple in page_data:
+        if tag in page_tuple[2]:
+            pages.append({'title': to_unicode(page_tuple[0]), 'url': to_unicode("../" + page_tuple[1])})
+    pages = sorted(pages, key=lambda t: t['title'])
+    write_to = Filepath(SITE_DIR + TAGS_DIR + to_string(tag))
+    ctx = Metadata(
+        title = "Tag: " + tag,
+        css = Filepath("css/minimal.css").relative_to(Filepath(TAGS_DIR + to_string(tag))).path,
+        license = "cc0",
+    )
+    env = Environment(loader=FileSystemLoader('.'))
+    page_list = env.get_template('templates/page-list.html')
+    body = to_unicode(page_list.render(pages=pages))
+    skeleton = env.get_template('templates/skeleton.html')
+    final = skeleton.render(body=body, page=ctx, css=ctx.css, path="../")
 
-    #with open(write_to.path, 'w') as f:
-        #f.write(to_string(final))
+    with open(write_to.path, 'w') as f:
+        f.write(to_string(final))
 
-## Make page with all tags
-##print("Creating page with all the tags")
-#env = Environment(loader=FileSystemLoader('.'))
-#page_list = env.get_template('templates/page-list.html')
-#pages = [{'title': to_unicode(tag), 'url': to_unicode(tag)} for tag in all_tags]
-#pages = sorted(pages, key=lambda t: t['title'])
-#body = to_unicode(page_list.render(pages=pages))
-#skeleton = env.get_template('templates/skeleton.html')
-#ctx = Metadata(
-    #title = "All tags",
-    #css = Filepath("css/minimal.css").relative_to(Filepath(TAGS_DIR + "index")).path,
-    #license = "cc0",
-#)
-#final = skeleton.render(page=ctx, body=body, css=ctx.css)
-#with open(SITE_DIR + TAGS_DIR + "index", 'w') as f:
-    #f.write(to_string(final))
+# Make page with all tags
+print("Creating page with all the tags")
+env = Environment(loader=FileSystemLoader('.'))
+page_list = env.get_template('templates/page-list.html')
+pages = [{'title': to_unicode(tag), 'url': to_unicode(tag)} for tag in all_tags]
+pages = sorted(pages, key=lambda t: t['title'])
+body = to_unicode(page_list.render(pages=pages))
+skeleton = env.get_template('templates/skeleton.html')
+ctx = Metadata(
+    title = "All tags",
+    css = Filepath("css/minimal.css").relative_to(Filepath(TAGS_DIR + "index")).path,
+    license = "cc0",
+)
+final = skeleton.render(page=ctx, body=body, css=ctx.css, path="./")
+with open(SITE_DIR + TAGS_DIR + "index", 'w') as f:
+    f.write(to_string(final))
 
-## Make page with all pages
-#print("Creating page with all the pages")
-#env = Environment(loader=FileSystemLoader('.'))
-#page_list = env.get_template('templates/page-list.html')
-#pages = [{'title': to_unicode(page_tup[0]), 'url': to_unicode(page_tup[1])} for page_tup in page_data]
-#pages = sorted(pages, key=lambda t: t['title'])
-#body = page_list.render(pages=pages)
-#skeleton = env.get_template('templates/skeleton.html')
-#ctx = Metadata(
-    #title = "All pages on the site",
-    #css = Filepath("css/minimal.css").relative_to(Filepath("all")).path,
-    #license = "cc0",
-#)
-#final = skeleton.render(page=ctx, body=body, css=ctx.css)
-#with open(SITE_DIR + "all", 'w') as f:
-    #f.write(to_string(final))
+# Make page with all pages
+print("Creating page with all the pages")
+env = Environment(loader=FileSystemLoader('.'))
+page_list = env.get_template('templates/page-list.html')
+pages = [{'title': to_unicode(page_tup[0]), 'url': to_unicode(page_tup[1])} for page_tup in page_data]
+pages = sorted(pages, key=lambda t: t['title'])
+body = page_list.render(pages=pages)
+skeleton = env.get_template('templates/skeleton.html')
+ctx = Metadata(
+    title = "All pages on the site",
+    css = Filepath("css/minimal.css").relative_to(Filepath("all")).path,
+    license = "cc0",
+)
+final = skeleton.render(page=ctx, body=body, css=ctx.css, path="../")
+with open(SITE_DIR + "all", 'w') as f:
+    f.write(to_string(final))
