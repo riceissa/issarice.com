aa9c96913c1f3ab919f4ae5ee62e7b72f7a25d48
Issa Rice <riceissa@gmail.com>
2015-07-07 02:35:13 -0700

    better

--- static/newlink.py
+++ static/newlink.py
@@ -4,6 +4,7 @@ import os
 import sys
 import subprocess
 import shlex
+from slugify import slugify_unicode
 
 def main():
     url = input("Enter URL for article: ").strip()
@@ -17,13 +18,14 @@ def main():
     datetime = run_command("date +'%Y-%m-%d at %I:%M %p'").strip()
     with open("wiki/articles-read.md", "r") as r:
         original = [line for line in r]
+    top_lines = find_top_lines(original) + 1
     log_if_v("Contents of wiki/articles-read.md read.")
     if open_editor:
         with open("wiki/articles-read.md", "w") as w:
-            for line in original[:37]:
+            for line in original[:top_lines]:
                 w.write(line)
             w.write(make_entry(datetime, url, title, ""))
-            for line in original[37:]:
+            for line in original[top_lines:]:
                 w.write(line)
         subprocess.call(["vim", "+42", "wiki/articles-read.md"])
     else:
@@ -37,9 +39,31 @@ def main():
                 w.write(line)
         log_if_v("File written.")
 
+def find_top_lines(lst):
+    '''
+    Return the third item in the list containing just "---\n". Return -1
+    if there are less than three items containing just "---\n".
+    '''
+    num_dash = 0
+    n = len(lst)
+    i = 0
+    while i < n and num_dash < 3:
+        if lst[i] == "---\n":
+            num_dash += 1
+        i += 1
+    if num_dash == 3:
+        return (i - 1)
+    else:
+        return -1
+
+def slug(s):
+    return slugify_unicode(s, to_lower=True)
+
 def make_entry(datetime, url, title, body):
+    anchor = slug(datetime)
     template = '''
-*{datetime}*
+
+*<a href="{anchor}" id={anchor}>{datetime}</a>*
 
 [{title}]({url})
 
@@ -48,8 +72,8 @@ def make_entry(datetime, url, title, body):
 ---
 
 '''
-    return (template.format(datetime=datetime, title=title, url=url,
-        body=body))
+    return (template.format(anchor=anchor, datetime=datetime, title=title,
+        url=url, body=body))
 
 def log_if_v(text, log=True):
     if log:
