d89e1f53f1ce37b71a4fb93e2965afc2e297b77f
Issa Rice <riceissa@gmail.com>
2015-07-29 21:02:40 -0700

    more serious documentain begins...

--- generator/tags.py
+++ generator/tags.py
@@ -1,3 +1,25 @@
+# This is free and unencumbered software released into the public domain.
+#
+# Anyone is free to copy, modify, publish, use, compile, sell, or distribute
+# this software, either in source code form or as a compiled binary, for any
+# purpose, commercial or non-commercial, and by any means.
+#
+# In jurisdictions that recognize copyright laws, the author or authors of this
+# software dedicate any and all copyright interest in the software to the public
+# domain. We make this dedication for the benefit of the public at large and to
+# the detriment of our heirs and successors. We intend this dedication to be an
+# overt act of relinquishment in perpetuity of all present and future rights to
+# this software under copyright law.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+# For more information, please refer to <http://unlicense.org/>
+
 class CyclePresenceException(Exception):
     """
     A cycle is present in the digraph, when it shouldn't be (for a DAG).
@@ -5,24 +27,66 @@ class CyclePresenceException(Exception):
     pass
 
 class TagNode(object):
+    """
+    Stores a node of a tag acyclic digraph. Supports aliases (synonyms).
+
+    Attributes:
+        canonical_name: A string containing the canonical form of the tag's
+            name.
+        aliases: A list of strings, where each string is an alias (synonym) of
+            the tag.
+    """
 
     def __init__(self, canonical_name, aliases=[]):
+        """
+        Initializes TagNode with a canonical_name and empty list of aliases.
+        """
         self.canonical_name = canonical_name
         self.aliases = aliases
 
     def add_alias(self, alias):
+        """
+        Add a new alias.
+
+        Args:
+            alias: A string that is a new alias.
+
+        Returns:
+            None.
+        """
         self.aliases.append(alias)
 
     def add_aliases(self, aliases):
+        """
+        Add a list of aliases to the node.
+
+        Args:
+            aliases: A list of strings; each string is a new alias.
+
+        Returns:
+            None.
+        """
         self.aliases.extend(aliases)
 
     def __repr__(self):
-        return "Tag({}, {})".format(self.canonical_name, self.aliases)
+        return "Tag('{}', {})".format(self.canonical_name.replace("'", "\\'"), self.aliases)
 
 class TagDag(object):
+    """
+    Represents a directed acyclic graph (DAG) of TagNodes.
+
+    Attributes:
+        data: A dictionary containing the DAG. Each key is a string of the
+            canonical tag name, and each value is a tuple (TagNode, [TagNode]),
+            where the first element is the TagNode object of the node (instead
+            of just the canonical name) and the second element is a list of
+            TagNodes containing all the parent nodes (i.e. immediate ancestors).
+        casing: A string stating how to deal with casing. Allowed options are:
+            "smart", "lower", "upper", "literal".
+    """
+
     def __init__(self, casing="smart"):
         """
-        For casing, allowed options are: "smart", "lower", "upper", "literal".
         """
         self.data = {}
 
@@ -47,8 +111,8 @@ class TagDag(object):
 
     def add_tag(self, tag):
         """
-        Add the TagNode to the DAG. Since adding nodes and adding edges
-        are separate processes, we don't have to perform any checks.
+        Add the TagNode to the DAG. Since adding nodes and adding edges are
+        separate processes, we don't have to perform any checks.
         """
         key = tag.canonical_name
         self.data[key] = (tag, [])
@@ -121,14 +185,19 @@ class TagDag(object):
     def add_parents(self, tag, parents):
         pass
 
-    def add_children(self, tag, children):
+    def add_children(self, tag: TagNode, children: '[TagNode]'):
+        """
+        """
         pass
 
     def is_descendant(self, tag_one, tag_two):
         """
         (TagNode, TagNode) -> bool
+
+        Check if tag_one is a descendant of tag_two.
         """
-        pass
+        is_des = False
+
 
     def implies(self, tag_one, tag_two):
         """
