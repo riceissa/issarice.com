ce3a664e9add1320f80adc679b4f352a15214b16
Issa Rice <riceissa@gmail.com>
2016-04-28 03:06:45 -0700

    new sitemap generator

---
+++ generator/sitemap.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cat <<EOF > _site/sitemap.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+EOF
+
+for file in wiki/*
+    do cat <<EOF  >> _site/sitemap.xml
+<url>
+  <loc>http://issarice.com/$(basename $file .md)</loc>
+  <changefreq>weekly</changefreq>
+</url>
+EOF
+done
+
+echo '</urlset>' >> _site/sitemap.xml
