3c9fe05b1b55b320011312ba6ba964d3c26ccd61
Issa Rice <riceissa@gmail.com>
2016-05-01 01:36:48 -0700

    fix grouping error

--- generator/all_pages.sh
+++ generator/all_pages.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-git ls-tree -r --name-only HEAD | while read filename; do
-    if [[ $filename == wiki/* ]] ;
-    then
-        title=`sed -n '2{p;q}' $filename | cut -c 8- | tr -d '"'`
-        echo "$(git log -1 --format="%ai" -- $filename) - [$title]($(basename $filename .md))"
-    fi
-done | sort -r | cut -d " " -f 4-
+# TODO: this is a hack
+
+echo "This is a list of all pages on this site, ordered by the date of last modification."
+echo ""
+{
+    git ls-tree -r --name-only HEAD | while read filename; do
+        if [[ $filename == wiki/* ]] ;
+        then
+            title=`sed -n '2{p;q}' $filename | cut -c 8- | tr -d '"'`
+            echo "$(git log -1 --format="%ai" -- $filename) - [$title]($(basename $filename .md))"
+        fi
+    done
+} | sort -r | cut -d " " -f 4-
