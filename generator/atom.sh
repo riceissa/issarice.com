8ca8763273dedbcea6a7f08059afe36fee0ae76e
Issa Rice <riceissa@gmail.com>
2017-04-30 23:08:46 -0700

    initial atom feed generator

---
+++ generator/atom.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+cat << EOF
+<?xml version="1.0" encoding="utf-8"?>
+<feed xmlns="http://www.w3.org/2005/Atom">
+  <title type="text">issarice.com</title>
+  <updated>$(date -Iseconds)</updated>
+  <author>
+    <name>Issa Rice</name>
+  </author>
+  <id>https://issarice.com/</id>
+  <link href="https://issarice.com/atom.xml" rel="self" type="application/atom+xml" />
+  <link rel="alternate" type="application/rss+xml" hreflang="en" href="http://issarice.com/rss.xml" />
+  <generator uri="https://github.com/riceissa/issarice.com/blob/master/generator/atom.sh">atom.sh</generator>
+EOF
+
+grep -m 1 -e '^date: ' wiki/* | \
+    sed -n 's/\([^:]\+\):date: \(.*\)/\2 \1/p' | \
+    sort -r | \
+    while read line; do
+        # echo $line | filename=`sed -n 's/[^ ] \(.*\)/\1/p'`
+        filename=`echo $line | sed -n 's/[^ ]\+ \(.*\)/\1/p'`
+        # echo $filename
+        title=`sed -n '2{p;q}' $filename | cut -c 8- | tr -d '"'`
+        base=$(basename $filename .md)
+        date=`echo $line | sed -n 's/\([^ ]\+\) \(.*\)/\1/p'`
+        echo "  <entry>"
+        echo "    <title>$title</title>"
+        echo '    <link href="https://issarice.com/'"$base"'"/>'
+        echo "    <id>tag:issarice.com,$date:/$base</id>"
+        echo "    <update>$date""T00:00:00$(date +'%:z')</updated>"
+        echo "  </entry>"
+
+        # Write the actual markdown bullet
+        # echo "- [$title]($base), $date"
+    done
+echo "</feed>"
