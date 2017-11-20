680212dc291aeefddb1b1303ae9c9fb7b8a5562a
Issa Rice <riceissa@gmail.com>
2017-11-19 19:07:26 -0800

    now documented in readme

--- generator/list_links.lua
+++ generator/list_links.lua
@@ -1,8 +1,5 @@
 -- License: CC0
 -- A simple Pandoc Lua filter to extract the links in a file.
--- To get all the links for my site, run:
---     rm -f links.txt && for filename in wiki/*; do pandoc -f markdown+smart -t html5 --mathjax $filename --lua-filter generator/url_filter.lua --lua-filter generator/list_links.lua -o temp >> links.txt; done && rm -f temp
--- You can then sort/make links unique as fit.
 
 function Link(elem)
   if elem.target:find("^https?://") then
