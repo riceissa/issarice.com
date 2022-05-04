991222d8cdcb3ac081a1760edfcd244cd6b5c621
Issa Rice <riceissa@gmail.com>
2022-05-04 14:29:33 -0700

    add more exceptions

--- generator/generate.py
+++ generator/generate.py
@@ -43,32 +43,50 @@ for filename in os.listdir("wiki"):
         # [[contact]], then the Pandoc Markdown reader will interpret that as a
         # link to the contact section, and you will end up with
         # "[[contact](#contact)]" instead of "[contact](contact)".
-        p = subprocess.run([
-            "pandoc", "-f", "markdown+smart-implicit_header_references",
-            "-t", "json", filepath
-        ], check=True, capture_output=True)
-        p2 = subprocess.run(["/home/issa/projects/pandoc-wikilinks-filter/wikilinks.py", "--base-url", "https://issarice.com/"], input=p.stdout, check=True, capture_output=True)
+        try:
+            p = subprocess.run([
+                "pandoc", "-f", "markdown+smart-implicit_header_references",
+                "-t", "json", filepath
+            ], check=True, capture_output=True)
+        except subprocess.CalledProcessError as e:
+            print("Error running pandoc (markdown to json):",
+                  "error code:", e.returncode,
+                  "error message:", e.stderr.decode("utf-8"), file=sys.stderr)
+
+        try:
+            p2 = subprocess.run(["/home/issa/projects/pandoc-wikilinks-filter/wikilinks.py", "--base-url", "https://issarice.com/"], input=p.stdout, check=True, capture_output=True)
+        except subprocess.CalledProcessError as e:
+            print("Error running wikilinks.py:",
+                  "error code:", e.returncode,
+                  "error message:", e.stderr.decode("utf-8"), file=sys.stderr)
+
         try:
             p_last_mod = subprocess.run(["git", "log", "-1", '--format=%ad', '--date=format:%Y-%m-%d', "--", filepath], check=True, capture_output=True)
             last_mod = p_last_mod.stdout.decode("utf-8").strip()
         except subprocess.CalledProcessError as e:
-            print("Error running git log command:", "error code:", e.returncode,
+            print("Error running git log command:",
+                  "error code:", e.returncode,
                   "error message:", e.stderr.decode("utf-8"), file=sys.stderr)
             sys.exit()
         print(subprocess.list2cmdline(p_last_mod.args))
         print(last_mod)
-        p3 = subprocess.run([
-            "pandoc", "-f", "json", "-t", "html5",
-            "--shift-heading-level-by", "1",
-            "--template", "templates/default.html5",
-            "-M", "toc-title:Contents",
-            "-M", "today:" + datetime.date.today().strftime("%Y-%m-%d"),
-            "-M", "lang:en",
-            "--toc", "--toc-depth", "4",
-            "--mathjax",
-            "--lua-filter", "generator/url_filter.lua",
-            "-M", "sourcefilename:" + shlex.quote(filepath),
-            "-M", "lastmodified:" + last_mod,
-            "-o", "_site/" + slugify(fileroot)
-        ], input=p2.stdout)
-        # print(subprocess.list2cmdline(p3.args))
+        try:
+            p3 = subprocess.run([
+                "pandoc", "-f", "json", "-t", "html5",
+                "--shift-heading-level-by", "1",
+                "--template", "templates/default.html5",
+                "-M", "toc-title:Contents",
+                "-M", "today:" + datetime.date.today().strftime("%Y-%m-%d"),
+                "-M", "lang:en",
+                "--toc", "--toc-depth", "4",
+                "--mathjax",
+                "--lua-filter", "generator/url_filter.lua",
+                "-M", "sourcefilename:" + shlex.quote(filepath),
+                "-M", "lastmodified:" + last_mod,
+                "-o", "_site/" + slugify(fileroot)
+            ], input=p2.stdout)
+            # print(subprocess.list2cmdline(p3.args))
+        except subprocess.CalledProcessError as e:
+            print("Error running pandoc (json to html5):",
+                  "error code:", e.returncode,
+                  "error message:", e.stderr.decode("utf-8"), file=sys.stderr)
