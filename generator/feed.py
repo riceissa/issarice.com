2bf82ded2e2ab7af36e728d1a315b2caab2e468b
Issa Rice <riceissa@gmail.com>
2022-05-25 09:56:32 -0700

    include xsl stylesheet

--- generator/feed.py
+++ generator/feed.py
@@ -22,6 +22,7 @@ def slugify(s):
 
 
 print("""<?xml version="1.0" encoding="utf-8"?>
+<?xml-stylesheet href="/feed.xsl" type="text/xsl"?>
 <feed xmlns="http://www.w3.org/2005/Atom">
   <title type="text">issarice.com</title>
   <updated>{}</updated>
